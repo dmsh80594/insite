13ee0827f74c50a24556565fda51072a59b2b42f
fragmashine <dmsh80594@gmail.com>
2019-03-07 12:44:10 +0300

    fix

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    new WOW().init();
 	$('[script-role="scroll-menu"]').on('click', function(event) {
         event.preventDefault();
         id = $(this).attr('href');
@@ -33,6 +34,45 @@ $(document).ready(function(){
     	change_lang(lang);
         $('[script-role="change-lang"]').removeClass('active');
         $(this).addClass('active');
+    })
+    $('.main_logo').click(function(e) {
+        e.preventDefault();
+        $('body').css("background-color", "#161616")
+        $('footer').css("background-color", "#161616")
+        $('.language-box').children().css("color", "#fff")
+        $('.nav').children().css("color", "#fff")
+        $('.logo').attr("src", "assets/img/insite.png")
+        $('.third_show').attr("src", "assets/img/4.png")
+        $('.border').css("background-color", "rgba(255, 255, 255, 0.04)")
+        $('.main_text_block > h1').css("color", "#fff")
+        $('.dev').css("background-color", "#EEFF30")
+        $('.shop').css("background-color", "#EEFF30")
+        $('.about').css("background-image", "url(../assets/img/bg_b.png)")
+        $('.about > h2').css("color", "#fff")
+        $('.about_text_block > p').css("color", "rgba(255, 255, 255, 0.48)")
+        $('.border_about').css("background-color", "rgba(255, 255, 255, 0.04)")
+        $('.desc_item > h3').css("color", "#fff")
+        $('.desc_item > p').css("color", "rgba(255, 255, 255, 0.48)")
+        $('.projects > h2').css("color", "#fff")
+        $('.projects_title_box > p').css("color", "rgba(255, 255, 255, 0.48)")
+        $('.link').css("border-bottom", "2px solid rgba(255, 255, 255, 0.04)")
+        $('.link > a').css("color", "#fff")
+        $('.projects_item_box_first').css("background-image", "url(../assets/img/11.png)")
+        $('.projects_item_box_sec').css("background-image", "url(../assets/img/22.png)")
+        $('.projects_item_box_third').css("background-image", "url(../assets/img/33.png)")
+        $('.projects_item_box_forth').css("background-image", "url(../assets/img/44.png)")
+        $('.projects_item_box_fifth').css("background-image", "url(../assets/img/55.png)")
+        $('.contacts > h2').css("color", "#fff")
+        $('.soc_box_item > a').css("color", "rgba(255, 255, 255, 0.48)")
+    })
+    $('.link').click(function(e) {
+        e.preventDefault();
+        $('.absolute_photo').css("display", "block")
+        var items = $('.absolute_photo img');
+        items.css('opacity', 0);
+        for (var i = 0; i < items.length; i++) {
+          $(items[i]).delay(i * 500).animate({ opacity: 1 }, 500);
+        }
     })
 });
 
